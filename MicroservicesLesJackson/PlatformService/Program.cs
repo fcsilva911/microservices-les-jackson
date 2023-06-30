63af024ed4f2986987be195d012a7bd422bd079f
fcsilva911 <fc.silva.911@gmail.com>
2023-06-30 23:00:22 +0100

    Add DB Context

--- MicroservicesLesJackson/PlatformService/Program.cs
+++ MicroservicesLesJackson/PlatformService/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("inMem"));
 builder.Services.AddControllers();
 
 var app = builder.Build();
