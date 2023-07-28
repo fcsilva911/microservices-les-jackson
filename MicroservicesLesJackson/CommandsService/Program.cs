fe4dc028eb45d5c98427ec649c3c880043d0b3d3
fcsilva911 <fc.silva.911@gmail.com>
2023-07-29 00:31:40 +0100

    Add commands service - data layer - AutoMapper Profiles

--- MicroservicesLesJackson/CommandsService/Program.cs
+++ MicroservicesLesJackson/CommandsService/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
