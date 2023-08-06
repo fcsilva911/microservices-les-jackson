2e7292f95a383b525c5b4c4990ab10b4902bea1c
fcsilva911 <fc.silva.911@gmail.com>
2023-08-07 00:59:26 +0100

    add MessageBusClient

---
+++ MicroservicesLesJackson/PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,45 @@
+﻿using PlatformService.DTOs;
+using RabbitMQ.Client;
+using System.Text.Json;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["port"]),
+            };
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+                _channel.ExchangeDeclare("trigger", ExchangeType.Fanout);
+                _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+                Console.WriteLine("--> Connected to MessageBus");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not connect to the message bus: {ex}");
+            }
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("--> RabbitMQ Connection Shutdown");
+        }
+
+        public void PublishNewPlatform(PlatformPublishedDto platformPublishedDto)
+        {
+            var message = JsonSerializer.Serialize(platformPublishedDto);
+        }
+    }
+}
