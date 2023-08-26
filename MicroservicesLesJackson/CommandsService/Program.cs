0f0b85eb2fa00fc920ae3a7b5f5a7e5e5377028e
fcsilva911 <fc.silva.911@gmail.com>
2023-08-27 00:42:53 +0100

    Add Event Listener to CommandService

--- MicroservicesLesJackson/CommandsService/Program.cs
+++ MicroservicesLesJackson/CommandsService/Program.cs
@@ -1,3 +1,4 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
@@ -8,7 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+
 builder.Services.AddControllers();
+
+builder.Services.AddHostedService<MessageBusSubscriber>();
+
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
